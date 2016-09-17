ca6bd93e204c6562e67eeb7f9546b52891c8b078
Jason Moggridge <jasonmoggridge@gmail.com>
2016-09-17 09:45:42 -0400

    Consolidated volume control scripts under hc-volume-control.sh
    Fixed bug that broke the mute command in hc-volume-control.sh

--- herbstluftwm/.config/herbstluftwm/hc-volume-control.sh
+++ herbstluftwm/.config/herbstluftwm/hc-volume-control.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-echo "HERE" >> /home/jason/here.txt
+case "$1" in
+    up)
+        amixer set Master 2%+ 
+    ;;
+
+    down)
+        amixer set Master 2%- 
+    ;;
+
+    mute) 
+        amixer set Master toggle 
+    ;;
+esac
+
+$HOME/.config/herbstluftwm/hc-input-dirty.sh

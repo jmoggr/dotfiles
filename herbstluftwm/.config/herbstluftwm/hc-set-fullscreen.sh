f43160d8fab1bdee026ceb56a0cf179a91d2f98e
Jason Moggridge <jasonmoggridge@gmail.com>
2016-09-22 13:21:02 -0400

    Added a script to maximize a frame, removing all other windows and frames from the current tag. Windows moved to the hidden tag are put qt the front of the queue so that they should come first if the user brings forward windows. While this fullscreen function is not undoable this eliminates the need for a fullscreen state and works transparently with all other functionality.

---
+++ herbstluftwm/.config/herbstluftwm/hc-set-fullscreen.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+current_tag=$(herbstclient attr tags.focus.name)
+hidden_tag="h$current_tag"
+hidden_layout=$(herbstclient dump $hidden_tag)
+focus_winid=$(herbstclient attr clients.focus.winid)
+
+for extra_winid in `herbstclient dump $current_tag | egrep -o  "0x[0-9a-z]{6,}" | grep -v "$focus_winid" | tac`; do
+    # add extra windids to start of hidden tag stack
+    hidden_layout=$(echo $hidden_layout | sed -r "s/(^[^ ]+ [^ ]+)(.*$)/\1 ${extra_winid}\2/")
+    window_moved=true
+done
+
+if [[ "$window_moved" == "true" ]]; then
+    herbstclient load $hidden_tag "$hidden_layout"
+    herbstclient load $current_tag "(clients max:0 ${focus_winid})"
+    $HOME/.config/herbstluftwm/hc-input-dirty.sh
+
+# even if no windows were moved there might still be empty frames
+elif herbstclient layout | grep max | egrep -v -q "0x[0-9a-z]{6,}"; then
+    herbstclient load $current_tag "(clients max:0 ${focus_winid})"
+
+    # no need to dirty
+fi
+
+# no need for ballancing

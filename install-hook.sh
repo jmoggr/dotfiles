62cd6a8aac68574ffa656265997ba8b1824b5046
Jason Moggridge <jasonmoggridge@gmail.com>
2016-10-01 12:09:49 -0400

    Added proper stow-all script

--- install-hook.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [[ -x "post-checkout" ]]; then
-	cp post-checkout $(git rev-parse --show-toplevel)/.git/hooks
-	source $(git rev-parse --show-toplevel)/.git/hooks/post-checkout
-fi
